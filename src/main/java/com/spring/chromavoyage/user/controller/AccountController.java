8d71ee8300f682225018a18c6368913746bd4810
hil679 <hil679922@gmail.com>
2023-08-18 00:55:21 +0900

    add: cors in login

--- src/main/java/com/spring/chromavoyage/user/controller/AccountController.java
+++ src/main/java/com/spring/chromavoyage/user/controller/AccountController.java
@@ -3,15 +3,13 @@ import com.spring.chromavoyage.user.response.SocialOAuthGoogleRes;
 import com.spring.chromavoyage.user.response.UserResponse;
 import com.spring.chromavoyage.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class AccountController {
     @Autowired
     UserService userService;
